81cedb6ac6a78577fba982efc6aba44750449af7
username917 <86450720+username917@users.noreply.github.com>
2022-07-08 22:59:32 -0400

    Add files via upload

---
+++ MyFirstSpringBootApp/src/main/java/fanshawe/MyFirstSpringBootApp/MyFirstSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package fanshawe.MyFirstSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootAppApplication.class, args);
+	}
+
+}
